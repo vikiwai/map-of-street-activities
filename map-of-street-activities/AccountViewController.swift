879e19fea2c85e5b41d46c1b431e8aabc406195d
Victoria Bunyaeva <avikiwai@gmail.com>
2019-04-08 21:51:32 +0300

    Add new scenes

---
+++ map-of-street-activities/AccountViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AccountViewController.swift
+//  map-of-street-activities
+//
+//  Created by vikiwai on 08/04/2019.
+//  Copyright © 2019 Victoria Bunyaeva. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
