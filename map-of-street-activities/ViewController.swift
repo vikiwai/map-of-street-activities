339ec970743ede1b4a23927c5c4871dc56ab6501
Victoria Bunyaeva <avikiwai@gmail.com>
2019-04-13 02:50:15 +0300

    Create variables for input e-mail and password in authorization form

--- map-of-street-activities/ViewController.swift
+++ map-of-street-activities/ViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var labelOfRequestRegistrationButton: UIButton!
+    @IBOutlet weak var inputEmailField: UITextField!
+    
+    @IBOutlet weak var inputPasswordField: UITextField!
     
     @IBAction func requestRegistration(_ sender: Any) {
         let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let newViewController = storyBoard.instantiateViewController(withIdentifier: "registrationController") as! RegistrationViewController
         self.present(newViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func requestEntry(_ sender: Any) {
+        
     }
     
     override func viewDidLoad() {
