a394febf4c0708e41246653c3ace8440b8e21a78
Victoria Bunyaeva <avikiwai@gmail.com>
2019-04-16 04:46:11 +0300

    Get page number from argv

--- server/loadData.js
+++ server/loadData.js
@@ -44,68 +44,80 @@ const kudagoCats = [
   'yarmarki-razvlecheniya-yarmarki'
 ];
 
-const saveKudagoEvents = (nPage) => {
-  console.log("Requesting page", nPage, "of events from KugaGo...");
-
-  let reqUrl = 'https://kudago.com/public-api/v1.4/events/'
-  reqUrl += '?fields=' + ['id', 'title', 'description', 'dates', 'place', 'images'].join(',');
-  reqUrl += '&expand=place';
-  reqUrl += '&page_size=100';
-  reqUrl += '&page='+ nPage;
-  reqUrl += '&location=msk';
-  reqUrl += '&categories=' + kudagoCats.join(',');
-
-  request(reqUrl, { json: true }, (err, res) => {
-    if(err) {
-      console.error(err);
+const saveAnEvent = eventInfo => {
+  return new Promise((resolve, reject) => {
+    if(!eventInfo.place) {
+      resolve(0);
       return;
     }
 
-    const { count, results } = res.body;
+    const tStart = eventInfo.dates[0].start;
+    const tEnd = eventInfo.dates[0].end;
 
-    console.log("Got", results.length, "events of the total", count);
+    let [startDate, startTime] = new Date(tStart * 1000).toISOString().split('T');
 
-    results.forEach(eventInfo => {
-      if(!eventInfo.place) {
-        console.log("No place:", eventInfo.id, eventInfo.title);
+    startTime = startTime.substr(0, startTime.lastIndexOf(':'));
+
+    const activityRecord = {
+      titleA: eventInfo.title,
+      locationName: eventInfo.place.address,
+      coordsLat: eventInfo.place.coords.lat,
+      coordsLon: eventInfo.place.coords.lon,
+      company: eventInfo.place.title,
+      wholeDescription: eventInfo.description,
+      date: startDate,
+      timeStart: startTime,
+      durationHours: (tEnd - tStart) / 3600,
+      creatorEmail: 'kudago',
+      kudagoId: eventInfo.id
+    };
+
+    const dbActivites = db.collection('activities');
+
+    dbActivites.findOne({ creatorEmail: 'kudago', kudagoId: eventInfo.id }).then(result => {
+      if(result) {
+          resolve(0)
+      }
+      else {
+        db.collection('activities').insertOne(activityRecord).then(result => {
+          if(result.result.ok) {
+            resolve(1);
+          }
+          else {
+            reject(result);
+          }
+        });
+      }
+    });
+  });
+};
+
+const savePageOfEvents = (nPage) => {
+  return new Promise((resolve, reject) => {
+    let reqUrl = 'https://kudago.com/public-api/v1.4/events/'
+    reqUrl += '?fields=' + ['id', 'title', 'description', 'dates', 'place', 'images'].join(',');
+    reqUrl += '&expand=place';
+    reqUrl += '&page_size=100';
+    reqUrl += '&page='+ nPage;
+    reqUrl += '&location=msk';
+    reqUrl += '&categories=' + kudagoCats.join(',');
+
+    request(reqUrl, { json: true }, (err, res) => {
+      if(err) {
+        console.error(err);
         return;
       }
 
-      const tStart = eventInfo.dates[0].start;
-      const tEnd = eventInfo.dates[0].end;
+      const { count, results } = res.body;
 
-      let [startDate, startTime] = new Date(tStart * 1000).toISOString().split('T');
+      console.log("Got", results.length, "events of the total", count);
 
-      startTime = startTime.substr(0, startTime.lastIndexOf(':'));
+      let nLeft = results.length;
 
-      const activityRecord = {
-        titleA: eventInfo.title,
-        locationName: eventInfo.place.address,
-        coordsLat: eventInfo.place.coords.lat,
-        coordsLon: eventInfo.place.coords.lon,
-        company: eventInfo.place.title,
-        wholeDescription: eventInfo.description,
-        date: startDate,
-        timeStart: startTime,
-        durationHours: (tEnd - tStart) / 3600,
-        creatorEmail: 'kudago',
-        kudagoId: eventInfo.id
-      };
-
-      const dbActivites = db.collection('activities');
-
-      dbActivites.findOne({ creatorEmail: 'kudago', kudagoId: eventInfo.id }).then(result => {
-        if(result) {
-            console.log("Already known:", eventInfo.id, eventInfo.title);
-            return;
-        }
-
-        db.collection('activities').insertOne(activityRecord).then(result => {
-          console.log("New event:", eventInfo.id, eventInfo.title);
-        });
+      Promise.all(results.map(saveAnEvent)).then(results => {
+        resolve(results.reduce((x, y) => x + y));
       });
     });
-
   });
 };
 
@@ -118,5 +130,13 @@ MongoClient.connect('mongodb://localhost:27017/', { useNewUrlParser: true }, (er
 
   console.log("Database connection OK");
 
-  saveKudagoEvents(2);
+  const nPage = parseInt(process.argv[2]);
+
+  console.log("Requesting page", nPage, "of events from KugaGo...");
+
+  savePageOfEvents(nPage).then(result => {
+    console.log("Loaded", result, "new events.");
+
+    process.exit();
+  });
 });
